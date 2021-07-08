5b3c09a27f70ba83b76c340441f3fcae27677e27
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-08 10:39:42 +0200

    v0.7

--- Exercice JavaScript  - snippet code/index.js
+++ Exercice JavaScript  - snippet code/index.js
@@ -549,4 +549,52 @@ li2.parentElement.insertBefore(last, li2)
 
 // ****************************************************** 
 
+// ##############################  18  ##########################################
+//  Les évènements 
 
+// Selectionne mon premier paragraphe 
+let first = document.querySelector("#demonstration1 p");
+
+first.addEventListener('click', function () {
+
+    // TOGGLE or ADD ? j'aurais pus mettre add au lieu de toggle, mais toggle permet au premier click de mettre la class et au second de l'enlever, la class add met la class, mais ne l'enleve pas au second click
+    first.classList.toggle('P--modifierJS')
+
+});
+
+// Selectionne tous mes P 
+let Paragraph = document.querySelectorAll('p');
+
+for (let i = 0; i < Paragraph.length; i++) {
+
+    let paragraphS = Paragraph[i];
+
+    (function(value) {
+
+        value.addEventListener('click', function() {
+
+            value.classList.toggle('P--modifierJS');
+
+        });
+
+    })(paragraphS);
+
+};
+
+// on utilise this, a la place de la fonction qui s'appelle elle meme elle permet de recuperer en gros l'index du p au moment du click, cette objet window de base, facilite lecriture du code dans ce contexte la. Personne ne force sont utilisation, au debut, je conseil de leviter surtout si vous avez du mal, apprenez a ecrire d'une facon dont vous vous comprenez, puis peaufiné avec l'experience. 
+for (let i = 0; i < Paragraph.length; i++) {
+
+    let paragraphS = Paragraph[i];
+    let orange = function() {
+
+        this.classList.toggle('P--modifierJS');
+       
+    };
+
+    paragraphS.addEventListener('click', orange);
+
+};
+
+
+
+// ****************************************************** 
