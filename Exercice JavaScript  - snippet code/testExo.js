dff051df5c97ac93c22f643305ac3275e8d9e71d
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-08 12:01:58 +0200

    v0.7

--- Exercice JavaScript  - snippet code/testExo.js
+++ Exercice JavaScript  - snippet code/testExo.js
@@ -25,5 +25,30 @@ for (let i = 0; i < pS.length; i++) {
 
 
 // demande de confirmation au click sur lien 
+let linkS = document.querySelectorAll('a.div--link');
+
+for (let i = 0; i < linkS.length; i++) {
+
+    let lien = linkS[i];
+
+    // le (e) ou (event) comme vous voulez fait reference a levenement qui a ete declanché au click, ou toute autre parametre a la place du click . preventDefault permet d'annuler lelement, si lutilisateur met annuler. Ca evite qu'il ce retrouve sur le lien, alors qu'il en a finalement pas envie xD
+    lien.addEventListener('click', function(event) {
+
+        let valeurDeWindowReponseUtilisateur = window.confirm('Voulez-vous réellement suivre ce liens frauduleux ? XD');
+        
+        let reponse = valeurDeWindowReponseUtilisateur;
+        
+        if (reponse === false) {
+
+            event.preventDefault();
+
+        }
+        
+
+    });
+  
+};
+
+
 
 // ****************************************************** 
