ae5bfa7b9a3f4d189e055595cfd83295bc391483
FALVO Anthony <anthony.falvo.pro@gmail.com>
2021-07-09 12:34:27 +0200

    v0.9

--- Exercice JavaScript  - snippet code/testExo.js
+++ Exercice JavaScript  - snippet code/testExo.js
@@ -1,8 +1,31 @@
 // ##############################  22  ##########################################
 // Les évènements -
 
-let input = document.querySelectorAll('#input1, #input2, #input3, #input4');
+//recupere une lettre tappé 
+let input = document.querySelector('#input1');
 
+input.addEventListener('keyup', function(e) {
+
+    console.log(e.key)
+
+});
+
+
+// si le nombre de caractere nest pas egale a 5, renvoie une erreur
+let listen = document.querySelector("#form");
+
+listen.addEventListener("submit", function(e) {
+
+    let content = document.querySelector("#cp");
+
+    if(content.value.length != 5) {
+
+        alert("le code postal n'est pas bon ");
+        e.preventDefault();
+
+    }
+
+})
 
 
 // ****************************************************** 
